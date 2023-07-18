f8e7a618f56bf9665770013fcc2fed0781091e06
Paul Winkler <slinkp@gmail.com>
2023-07-18 12:53:01 -0700

    Add skeleton main root node

---
+++ main.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	restart_game()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func restart_game():
+	print("Restarting")
+
+
