b3d6d0369386431cf903aa01aca989ecab77445d
Paul Winkler <slinkp@gmail.com>
2023-07-21 10:58:12 -0700

    Beginning of more sophisticated pitch control: Quantizing to semitones

--- main.gd
+++ main.gd
@@ -2,7 +2,7 @@ extends Node
 
 @export var ball_scene: PackedScene
 
-@export var speed = 3000.0
+@export var speed = 1000.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- wall.gd
+++ wall.gd
@@ -9,8 +9,13 @@ var bus_mapping = {
 }
 var player: AudioStreamPlayer
 
+# Twelfth root of two = one semitone in equal temperament.
+# See eg https://dbpedia.org/page/Twelfth_root_of_two
+var semitone_freq_ratio: float = pow(2.0, (1.0 / 12.0))
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("Semitone freq ratio: %s" % semitone_freq_ratio)
 	load_sound(name)
 	pass # Replace with function body.
 
@@ -24,10 +29,27 @@ func handle_hit(hit_position):
 	make_sound(hit_position)
 
 
+func pitch_scale_from_x_position(x : int) -> float:
+	# Spread some octaves over the x coords.
+	var semitone_range = 24
+	# Center of wall = base pitch of sample.
+	# Right end = 1 octave up, left end = 1 octave down.
+	# So, map position x to the range (-12, 12)
+	var screen_width: float = 1100.0  # TODO get from viewport
+	var semitone = (x / screen_width) * semitone_range
+	semitone = int(round(semitone))
+	# Offset so center of screen is sample's base pitch, ie
+	# ie center is zero semitones = unchanged from orig sample.
+	semitone -= (semitone_range / 2)
+	print("Got semitone %s from position %s" % [semitone, x])
+	var pitch_ratio: float = pow(semitone_freq_ratio, semitone)
+	print("Got pitch ratio %s from position %s" % [pitch_ratio, x])
+	return pitch_ratio
+
+
 func make_sound(hit_position):
-	# print("Beep! %s" % name)
-	var pitch_scale = randfn(0.5, 2.0)
-	# print("Stream is %s" % player.has_stream_playback())
+	# TODO: make this work for y on the left/right walls too
+	var pitch_scale = pitch_scale_from_x_position(hit_position.x)
 	player.pitch_scale = pitch_scale
 	player.play()
 
